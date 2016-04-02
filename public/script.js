f2ed5a043d79f657e223d575e344296e2409382c
kpharasi <kartik.230@gmail.com>
2016-04-02 01:07:42 -0400

    Testing purpose 3

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     
     var pad = document.getElementById('pad');
-    
-    sharejs.open(document.location.pathname, 'text', function(error, doc) {
+
+    sharejs.open('home', 'text', function(error, doc) {
         
         doc.attach_textarea(pad);
     });
